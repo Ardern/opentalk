8a11459fee2378abd11ce2445fb33b3b913fd56c
Moringa School <samcodespace@gmail.com>
2022-02-16 08:41:29 +0300

    adds authentication pages

--- website/__init__.py
+++ website/__init__.py
@@ -14,7 +14,9 @@ def create_app():
     app.config['SECRET_KEY'] = "helloworld"
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     
     return app
