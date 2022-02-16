e45f7545fcff07d4bb1492589771a722b216d75d
Moringa School <samcodespace@gmail.com>
2022-02-16 15:18:35 +0300

    adds posts model and views

--- website/__init__.py
+++ website/__init__.py
@@ -6,26 +6,19 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-# def create_app():
-#     app = Flask(__name__)
-#     app.config["SECRET_KEY"] = "helloworld"
-
-#     return app
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'squlite:///[DB_NAME]'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
-  
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
+    from .models import User, Post
 
     create_database(app)
 
@@ -37,8 +30,8 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    
     return app
+
 
 def create_database(app):
     if not path.exists("website/" + DB_NAME):
